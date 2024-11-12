8a301496570433c1b403f3d30536b942e6a9afa8
aneeshcheriank <aneeshcheriank@gmail.com>
2024-11-12 21:32:21 +0000

    changed the model

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 
+MODEL = "openai-community/gpt2"
+
 def generate(content):
-    generator = pipeline(model="HuggingFaceH4/zephyr-7b-beta")
+    generator = pipeline(model=MODEL)
     # model in ~/.cache/huggingface/hub
-    # Zephyr-beta is a conversational model, so let's pass it a chat instead of a single string
-    return generator([{"role": "user", "content": content}], do_sample=False, max_new_tokens=2)
+    return generator(content)
